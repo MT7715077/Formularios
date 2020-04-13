8f532e3a5bf537c9c88861a2a2ca1acefedab16e
MT7715077 <megamegamegaomega@gmail.com>
2020-04-13 00:39:53 -0400

    TERCER COMMIT

--- validaciones.js
+++ validaciones.js
@@ -5,3 +5,62 @@
  */
 
 
+function validarForma(forma){
+    var usuario = forma.usuario;
+    if (usuario.value == "" ||  usuario.value == "Escribir usurio") {
+        alert("Debe proporcionar un nombre de usuario");
+        usuario.focus();
+        usuario.select();
+        return false;
+        
+    }
+    var password = forma.apassword;
+    if (password.value == "" ||  password.value.length < 3 ) {
+        alert("Debe prporcionar un password al menos de tres caracateres");
+        password.focus();
+        password.select();
+        return false;
+        
+        
+    }
+    var  tecnologias = forma.tecnologias;
+    var  checkSeleccionado = false;
+    for (var i = 0; i < tecnologias.length; i++) {
+        if (tecnologias[i].checked) {
+            checkSeleccionado = true;
+            
+        }
+        
+    }
+    if (!checkSeleccionado) {
+        alert("Debe selecionar alguna tecnologia");
+        return false;
+        
+    }
+    var generos = forma.genero;
+    var radioSeleccionado = false;
+    for (var i = 0; i < generos.length; i++) {
+        if (generos[i].checked) {
+            radioSeleccionado = true;
+            
+        }
+        
+        
+    }
+    if (!radioSeleccionado) {
+        alert("El usuario debe sellecionar un genero");
+        return false;
+        
+    }
+    var ocupacion = forma.ocupacion;
+    if (ocupacion.value == "") {
+        alert("Debe seleccionar una ocupacion");
+        return false;
+        
+    }
+    
+    alert("Formulario valido, enviando datos....");
+    return true;
+    
+}
+
